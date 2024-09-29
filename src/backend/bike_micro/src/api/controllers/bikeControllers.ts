6df99b8d2abadfa2e32394b5166297cb82aea6d2
PapaLeoneIV <riccardo.leone97@gmail.com>
2024-09-29 21:50:23 +0200

    [bike micro] implement logic using entity and REPOSITORY PATTERN (will need to apply to other services)

--- src/backend/bike_micro/src/api/controllers/bikeControllers.ts
+++ src/backend/bike_micro/src/api/controllers/bikeControllers.ts
@@ -1,28 +1,22 @@
 import { Request, Response } from "express";
 import { z } from "zod";
 import {
-  check_bikes_availability,
-  revert_bike_order,
+  BikeOrdersManager,
+  BikeDBManager,
+  BikeOrder,
 } from "../service/bikeService";
 
-const URL_order_management = "http://localhost:3003/order/bike_update";
-
 const order_schema = z.object({
-  road: z.string(),
-  dirt: z.string(),
+  id: z.string(),
+  road_bike_requested: z.string(),
+  dirt_bike_requested: z.string(),
 });
-
-interface BikeRequested {
-  road: string;
-  dirt: string;
-}
-let bike_requested: BikeRequested;
 
 export const receive_order = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  //PARSE DATA
+    /*TODO improve parsing */
   let parsedBody: any;
   try {
     parsedBody = order_schema.parse(req.body.bikes);
@@ -31,24 +25,23 @@ export const receive_order = async (
     console.log("Error parsing data: request body not valid!", error);
     return;
   }
-  try {
-    console.log("Data received:", parsedBody);
-    bike_requested = {
-      road: parsedBody.road,
-      dirt: parsedBody.dirt,
-    };
-    const db_response = await check_bikes_availability(bike_requested);
-    //RESPOND TO Order Management
-    if (db_response) {
-      res.send("BIKEAPPROVED");
-    } else {
-      res.send("BIKEDENIED");
-    }
-  } catch (error) {
-    console.log("Error parsing data: request body not valid!", error);
-    res.status(400).json({ error: "Bad Request" });
-    return;
+
+  const manager_ordini = new BikeOrdersManager();
+  const manager_db = new BikeDBManager();
+  let bike_order = new BikeOrder(parsedBody);
+
+  manager_ordini.createBikeOrder(bike_order);
+  let available_dirt_bikes = await manager_db.getNumberOfDirtBikes();
+  let available_road_bikes = await manager_db.getNumberOfRoadBikes();
+  if (
+    available_dirt_bikes >= bike_order.dirt_bike_requested &&
+    available_road_bikes >= bike_order.road_bike_requested
+  ) {
+    manager_ordini.updateBikeOrderStatus(bike_order.info.id, "APPROVED");
+  } else {
+    manager_ordini.updateBikeOrderStatus(bike_order.info.id, "DENIED");
   }
+  res.send(`BIKEORDER'${bike_order.info.renting_status}'`); 
 };
 
 export const revert_order = async (
@@ -57,6 +50,8 @@ export const revert_order = async (
 ): Promise<void> => {
   console.log("Reverting order...");
   let parsedBody: any;
+  
+  /*TODO improve parsing */
   try {
     parsedBody = order_schema.parse(req.body.bikes);
   } catch (error) {
@@ -64,22 +59,19 @@ export const revert_order = async (
     console.log("Error parsing data: request body not valid!", error);
     return;
   }
-  try {
-    console.log("Data received:", parsedBody);
-    bike_requested = {
-      road: parsedBody.road,
-      dirt: parsedBody.dirt,
-    };
-    const db_response = await revert_bike_order(bike_requested);
-    //RESPOND TO Order Management
-    if (db_response) {
-      res.send("BIKEORDERREVERTED");
-    } else {
-      res.send("BIKEORDERNOTREVERTED");
-    }
-  } catch (error) {
-    console.log("Error parsing data: request body not valid!", error);
-    res.status(400).json({ error: "Bad Request" });
+
+
+  const manager_ordini = new BikeOrdersManager();
+  const manager_db = new BikeDBManager();
+  let bike_order = await manager_ordini.getBikeOrderById(parsedBody.id);
+  if (!bike_order) {
+    res.status(404).json({ error: "Bike order not found" });
     return;
   }
+  manager_db.incrementBikeCount(
+    parsedBody.road_bike_requested,
+    parsedBody.dirt_bike_requested
+  );
+  manager_ordini.deleteBikeOrder(bike_order.id);
+  res.send("BIKEORDERREVERTED");
 };
--- src/backend/bike_micro/src/api/service/bikeService.ts
+++ src/backend/bike_micro/src/api/service/bikeService.ts
@@ -1,86 +1,158 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-interface BikeRequested {
-    road: string;
-    dirt: string;
-}
-
 interface bike_order{
-    id : number;
-    road: number;
-    dirt: number;
+    id : string;
+    road_bike_requested: string;
+    dirt_bike_requested: string;
     renting_status: string;
     created_at: Date;
     updated_at: Date;
 }
 
+export class BikeOrder {
+
+    public info: bike_order;
+
+    constructor(info: bike_order) {
+        this.info = info;
+    }
+
+    public get id(): string {
+        return this.info.id;
+    }
+
+    public get road_bike_requested(): number {
+        return parseInt(this.info.road_bike_requested, 10);
+    }
+
+    public get dirt_bike_requested(): number {
+        return parseInt(this.info.dirt_bike_requested, 10);
+    }
+
+    public get renting_status(): string {
+        return this.info.renting_status;
+    }
+
+    public get created_at(): Date {
+        return this.info.created_at;
+    }
+
+    public get updated_at(): Date {
+        return this.info.updated_at;
+    }
+
+}
 
 
+export class BikeOrdersManager {
+    async getBikeOrderById(id: string): Promise<BikeOrder> {
+        const bike_order  = await prisma.order.findUnique({
+            where: {
+                id: id,
+            },
+        }) || null;
 
-export const check_bikes_availability = async (req: BikeRequested): Promise<boolean> => {
-    try {
+        if (!bike_order) {
+            throw new Error(`Bike order with id ${id} not found`);
+        }
+        return new BikeOrder(bike_order);
+    }
+
+    async createBikeOrder(bike_order: BikeOrder): Promise<BikeOrder> {
+        const new_bike_order = await prisma.order.create({
+            data: {
+                road_bike_requested: bike_order.info.road_bike_requested,
+                dirt_bike_requested: bike_order.info.dirt_bike_requested,
+                renting_status: bike_order.info.renting_status,
+                created_at: bike_order.info.created_at,
+                updated_at: bike_order.info.updated_at,
+            },
+        });
+        return new BikeOrder(new_bike_order);
+    }
+
+    async updateBikeOrder(bike_order: BikeOrder): Promise<BikeOrder> {
+        const updated_bike_order = await prisma.order.update({
+            where: {
+                id: bike_order.info.id,
+            },
+            data: {
+                road_bike_requested: bike_order.info.road_bike_requested,
+                dirt_bike_requested: bike_order.info.dirt_bike_requested,
+                renting_status: bike_order.info.renting_status,
+                created_at: bike_order.info.created_at,
+                updated_at: bike_order.info.updated_at,
+            },
+        });
+        return new BikeOrder(updated_bike_order);
+    }
+
+    async updateBikeOrderStatus(id: string, status: string): Promise<BikeOrder> {
+        const updated_bike_order = await prisma.order.update({
+            where: {
+                id: id,
+            },
+            data: {
+                renting_status: status,
+            },
+        });
+        return new BikeOrder(updated_bike_order);
+    }
+
+    async deleteBikeOrder(id: string): Promise<void> {
+        await prisma.order.delete({
+            where: {
+                id: id,
+            },
+        });
+    }
+}
+
+
+export class BikeDBManager {
+    async getNumberOfRoadBikes(): Promise<number> {
         const road_bikes_count = await prisma.bikes.aggregate({
             _sum: {
                 road: true,
             },
         });
+        return road_bikes_count._sum.road ?? 0;
+    }
 
+    async getNumberOfDirtBikes(): Promise<number> {
         const dirt_bikes_count = await prisma.bikes.aggregate({
             _sum: {
                 dirt: true,
             },
         });
-
-        const available_road_bikes = road_bikes_count._sum.road ?? 0;
-        const available_dirt_bikes = dirt_bikes_count._sum.dirt ?? 0;
-        
-        const requested_road_bikes = parseInt(req.road, 10);
-        const requested_dirt_bikes = parseInt(req.dirt, 10);
-        console.log("Requested road bikes:", requested_road_bikes);
-        console.log("Requested dirt bikes:", requested_dirt_bikes);
-
-        if (requested_road_bikes <= available_road_bikes 
-            && requested_dirt_bikes <= available_dirt_bikes) {
-            console.log("Bikes are available, updating database...");
-            await prisma.bikes.updateMany({
-                data: {
-                    road: {
-                        decrement: requested_road_bikes,
-                    },
-                    dirt: {
-                        decrement: requested_dirt_bikes,
-                    },
-                },
-            });
-            console.log("Database successfully updated.");
-            return true;
-        }
-
-        console.log("Not enough bikes available.");
-        return false; 
-    } catch (error) {
-        console.log("Error checking bike availability:", error);
-        return false; 
+        return dirt_bikes_count._sum.dirt ?? 0;
     }
-};
 
-export const revert_bike_order = async (req: BikeRequested): Promise<boolean> => {
-    try {
+    async incrementBikeCount(road_bikes: number, dirt_bikes: number): Promise<void> {
         await prisma.bikes.updateMany({
             data: {
                 road: {
-                    increment: parseInt(req.road, 10),
+                    increment: road_bikes,
                 },
                 dirt: {
-                    increment: parseInt(req.dirt, 10),
+                    increment: dirt_bikes,
                 },
             },
         });
-        console.log("Database successfully updated.");
-        return true;
-    } catch (error) {
-        console.log("Error reverting bike order:", error);
-        return false;
     }
+
+    async decrementBikeCount(road_bikes: number, dirt_bikes: number): Promise<void> {
+        await prisma.bikes.updateMany({
+            data: {
+                road: {
+                    decrement: road_bikes,
+                },
+                dirt: {
+                    decrement: dirt_bikes,
+                },
+            },
+        });
+    }
+
 }
