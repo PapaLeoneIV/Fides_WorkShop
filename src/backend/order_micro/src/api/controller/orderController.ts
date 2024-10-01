017f6d89bff2d2db7821cf78168d7459421ada69
PapaLeoneIV <riccardo.leone97@gmail.com>
2024-10-01 23:06:28 +0200

    [order micro] first implementation of logic

--- src/backend/order_micro/src/api/controller/orderController.ts
+++ src/backend/order_micro/src/api/controller/orderController.ts
@@ -1,59 +1,45 @@
-import { Request, Response } from 'express';
-import { order_context } from '../service/orderService';
-import { z } from 'zod';
+import { Request, Response } from "express";
+import { order_context, order_info } from "../service/orderService";
+import { z } from "zod";
 
-interface order_info {
-  bikes: { road: string; dirt: string },
-  hotel: { from: Date; to: Date; room: number },
-  payment_info: { amount: string }
-}
 
-const parse_request = (body: any) => {
-  const orderSchema = z.object({
-    order: z.object({
-      payment_info: z.object({
-        amount: z.string()
-      }),
-      bikes: z.object({
-        road: z.string(),
-        dirt: z.string()
-      }),
-      hotel: z.object({
-        to: z.string(),
-        from: z.string(),
-        room: z.string()
-      })
-    })
+  const order_schema = z.object({
+    amount: z.string(),
+    road_bike_requested: z.string(),
+    dirt_bike_requested: z.string(),
+    to: z.string(),
+    from: z.string(),
+    room: z.string(),
+
   });
+  const parseOrderWithDefaults = (data: any) => {
+    const parsedData = order_schema.parse(data);
+    return {
+      ...parsedData,
+      order_status: "PENDING",
+      created_at: new Date(),
+      updated_at: new Date(),
+    } as order_info;
+  };
+
+export const handler_book_vacation = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  let request_body: order_info;
   try {
-    return orderSchema.parse(body);
+    console.log(req.body);
+    request_body = parseOrderWithDefaults(req.body);
   } catch (error) {
+    res.status(400).json({ error: "Bad Request" });
     console.log("Error parsing data: request body not valid!", error);
-    throw new Error("Bad Request");
+    return;
   }
+  /*TODO qui devo creare il nuovo ordine con tutte le info e poi passare quello a order_context */
+
+  const order = new order_context(request_body);
+  order.order = await order.manager_db.create_order(order.order);
+  order.process_order(order.order);
+
+  res.status(202).send("Order is being processed");
 };
-
-export const handler_book_vacation = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const parsedBody = parse_request(req.body) as order_info;
-
-    let { bikes, hotel, payment_info } = parsedBody;
-    
-    /*TODO qui devo creare il nuovo ordine con tutte le info e poi passare quello a order_context */
-
-
-
-    const order = new order_context(bikes, hotel, payment_info);
-    order.process_order(bikes, hotel, payment_info);
-
-    res.status(202).send("Order is being processed");
-  } catch (error: any) {
-    if (error.message === "Bad Request") {
-      res.status(400).json({ error: "Bad Request" });
-    } else {
-      console.log('Error processing order:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  }
-};
-
--- src/backend/order_micro/src/api/service/orderService.ts
+++ src/backend/order_micro/src/api/service/orderService.ts
@@ -1,36 +1,112 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 import axios from "axios";
 //BASE CLASS--------------------------------------------------------------
-interface order_info {
-  bikes: { road: string; dirt: string },
-  hotel: { from: Date; to: Date; room: number },
-  payment_info: { amount: string }
+export interface order_info {
+  id: string;
+  from: string;
+  to: string;
+  room: string;
+  amount: string;
+  road_bike_requested: string;
+  dirt_bike_requested: string;
+  order_status: string;
+  created_at: Date;
+  updated_at: Date;
 }
 
 interface OrderState {
-  handle_request(
-    context: order_context,
-    info: order_info
-  ): Promise<void>;
+  handle_request(context: order_context, info: order_info): Promise<void>;
+}
+class OrderManagerDB {
+  async create_order(info: order_info): Promise<order_info> {
+    const x  =  await prisma.order.create({
+      data: {
+        from: info.from, 
+        to: info.to,
+        room: info.room,
+        road_bike_requested: info.road_bike_requested,
+        dirt_bike_requested: info.dirt_bike_requested,
+        amount: info.amount,
+        order_status: "PENDING",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    });
+    return x;
+  }
+
+  async check_existance(id: string): Promise<boolean> {
+    const order = await prisma.order.findFirst({
+      where: {
+        id: id,
+      },
+    });
+    if (!order) {
+      console.log(`Order with id ${id} not found`);
+      return false;
+    }
+    return true;
+  }
+
+  async update_status(id: string, status: string): Promise<void> {
+    await prisma.order.update({
+      where: {
+        id: id,
+      },
+      data: {
+        order_status: status,
+      },
+    });
+    return;
+  }
+
+  async update_order(info: order_info): Promise<void> {
+    await prisma.order.update({
+      where: {
+        id: info.id,
+      },
+      data: {
+        to: info.to,
+        from: info.from,
+        room: info.room,
+
+        road_bike_requested: info.road_bike_requested,
+        dirt_bike_requested: info.dirt_bike_requested,
+
+        amount: info.amount,
+
+        order_status: "PENDING",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    });
+    return;
+  }
 }
 /**I develop this class following the STATE PATTERN DESIGN, where each state has
  * a set of different behaviours. The main idea is to have a context that will
  * change its state depending on the response of the different services. The context
- * will handle the request and will call the different services to process the order. 
+ * will handle the request and will call the different services to process the order.
  * In doing so i tried to mix this STATE pattern with the SAGA PATTERN , where we have
  * a pipeline of services that will be called one after the other. If one of the services
  * fails, we will revert the order and send the response to the UI.
  */
+
+
+
 export class order_context {
   private state: OrderState;
-  private info: order_info;
+  public manager_db: OrderManagerDB;
+  public order: order_info;
 
-  constructor(
-    info: order_info
-  ) {
-    this.info = info;
+  constructor(info: order_info) {
+    this.manager_db = new OrderManagerDB();
     this.state = new StartOrderState();
+    this.order = info;
   }
-
+  
   getState() {
     return this.state;
   }
@@ -39,26 +115,25 @@ export class order_context {
     this.state = state;
   }
 
-  /**Each one of the different state will behave differently but all of them
-   * will call this method to process the order
-   */
-  async process_order(
-    info: order_info
-  ) {
-    await this.state.handle_request(this, info);
+  async process_order(order: order_info) {
+    await this.state.handle_request(this, order);
   }
 
-
-  async sendRequestToBikeShop(bikes: {
-    road: string;
-    dirt: string;
-  }): Promise<string> {
+  async sendRequestToBikeShop(
+    order: order_info,
+  ): Promise<string> {
     /*TODO implement POST request to bike */
     console.log("sending request to bikeShop!");
     try {
+      const bikes = {
+        order_id: order.id,
+        road_bike_requested: order.road_bike_requested,
+        dirt_bike_requested: order.dirt_bike_requested,
+      };
+      console.log(bikes);
       const response: any = await axios.post(
         "http://localhost:3000/bike_renting/send_data",
-        { bikes }
+        bikes
       );
       return response.data;
     } catch (error) {
@@ -67,13 +142,17 @@ export class order_context {
     }
   }
 
-  async sendRequestToHotel(hotel: {
-    from: Date;
-    to: Date;
-    room: number;
-  }): Promise<string> {
+  async sendRequestToHotel(
+  order: order_info,
+  ): Promise<string> {
     console.log("sending request to hotel service!");
     try {
+      const hotel = {
+        order_id: order.id,
+        from: order.from,
+        to: order.to,
+        room: order.room,
+      };
       const response: any = await axios.post(
         "http://localhost:3001/hotel_booking/send_data",
         { hotel }
@@ -85,11 +164,12 @@ export class order_context {
     }
   }
 
-  async sendRequestToMoney(payment_info: {
-    amount: string;
-  }): Promise<string> {
+  async sendRequestToMoney(order: order_info ): Promise<string> {
     console.log("sending request to money service!");
     try {
+      const payment_info = {
+        amount: order.amount,
+      };
       const response: any = await axios.post(
         "http://localhost:3002/payment/send_data",
         { payment_info }
@@ -101,12 +181,18 @@ export class order_context {
     }
   }
 
-  async revertBikeOrder(bikes: {
-    road: string;
-    dirt: string;
-  }): Promise<string> {
+
+  /*TODO modify it so that revert endpoint will only accept the orderid */
+  async revertBikeOrder(
+    road_bike_requested: string,
+    dirt_bike_requested: string,
+  ): Promise<string> {
     console.log("reverting bike order!");
     try {
+      const bikes = {
+        road: road_bike_requested,
+        dirt: dirt_bike_requested,
+      };
       const response: any = await axios.post(
         "http://localhost:3000/bike_renting/revert_order",
         {
@@ -120,13 +206,18 @@ export class order_context {
     }
   }
 
-  async revertHotelOrder(hotel: {
-    from: Date;
-    to: Date;
-    room: number;
-  }): Promise<string> {
+  async revertHotelOrder(
+    from: string,
+    to: string,
+    room: string,
+  ): Promise<string> {
     console.log("reverting hotel order!");
     try {
+      const hotel = {
+        from: from,
+        to: to,
+        room: room,
+      };
       const response: any = await axios.post(
         "http://localhost:3001/hotel_booking/revert_order",
         {
@@ -143,8 +234,8 @@ export class order_context {
   /*TODO implement the response to UI */
 }
 
-
 //DIFFERENT STATES with different behaviours--------------------------------------
+
 /**This is the inital state of the order, it will forward the request to the dedicated service.
  * If the response is positive, it will change the state to ItemsConfirmedState, otherwise
  * it will change the state to ItemsDeniedState.
@@ -152,27 +243,29 @@ export class order_context {
 class StartOrderState implements OrderState {
   async handle_request(
     context: order_context,
-    info: order_info
   ): Promise<void> {
     console.log("Order is in pending state, processing...");
+    context.manager_db.create_order(context.order);
     try {
       const [bike_response, hotel_response] = await Promise.all([
-        await context.sendRequestToBikeShop(info.bikes),
-        await context.sendRequestToHotel(info.hotel),
+        await context.sendRequestToBikeShop(context.order),
+        await context.sendRequestToHotel(context.order),
       ]);
       if (
         bike_response === "BIKEAPPROVED" &&
         hotel_response === "HOTELAPPROVED"
       ) {
         context.setState(new ItemsConfirmedState());
-        context.process_order(info);
+        context.process_order(context.order);
+        context.manager_db.update_status(context.order.id, "ITEMS_CONFIRMED");
       } else {
         const failureInfo = {
           bikeFailed: bike_response !== "BIKEAPPROVED",
           hotelFailed: hotel_response !== "HOTELAPPROVED",
         };
         context.setState(new ItemsDeniedState(failureInfo));
-        context.process_order(info);
+        context.process_order(context.order);
+        context.manager_db.update_status(context.order.id, "ITEMS_DENIED");
       }
     } catch (error) {
       console.log("Error in sending order!");
@@ -188,17 +281,19 @@ class StartOrderState implements OrderState {
 class ItemsConfirmedState implements OrderState {
   async handle_request(
     context: order_context,
-    info: order_info
+    order: order_info
   ): Promise<void> {
     console.log("Items are confirmed, waiting for payment...");
     try {
-      const response = await context.sendRequestToMoney(info.payment_info);
+      const response = await context.sendRequestToMoney(context.order);
       if (response === "PAYMENTAPPROVED") {
         context.setState(new PaymentAcceptedState());
-        context.process_order(info);
+        context.process_order(context.order);
+        context.manager_db.update_status(order.id, "APPROVED");
       } else {
         context.setState(new PaymentDeniedState());
-        context.process_order(info);
+        context.process_order(context.order);
+        context.manager_db.update_status(order.id, "PAYMENT_DENIED");
       }
     } catch (error) {
       console.log("Error in sending order!");
@@ -220,19 +315,18 @@ class ItemsDeniedState implements OrderState {
 
   async handle_request(
     context: order_context,
-    info: order_info
-
+    order: order_info
   ): Promise<void> {
     if (this.failureInfo.bikeFailed) {
       console.log("Bike failed, reverting hotel order...");
-      const response: string = await context.revertHotelOrder(info.hotel);
+      const response: string = await context.revertHotelOrder(order.from, order.to, order.room);
       if (response === "HOTELORDERREVERTED") {
         console.log("Reverted hotel order!");
       }
     }
     if (this.failureInfo.hotelFailed) {
       console.log("Hotel failed, reverting bike order...");
-      const response: string = await context.revertBikeOrder(info.bikes);
+      const response: string = await context.revertBikeOrder(order.road_bike_requested, order.dirt_bike_requested);
       if (response === "BIKEORDERREVERTED") {
         console.log("Reverted bike order!");
       }
@@ -244,11 +338,11 @@ class ItemsDeniedState implements OrderState {
 /**We reach this stage when everything down the pipeline went well, the only thing left is
  * to send the response to the UI.
  */
+
 class PaymentAcceptedState implements OrderState {
   async handle_request(
     context: order_context,
     info: order_info
-
   ): Promise<void> {
     console.log("Payment was succesfull!");
     /*TODO send response to UI */
