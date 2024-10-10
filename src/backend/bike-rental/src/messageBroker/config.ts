fa44971d4cd90bfe61f3f24027da626dfc691b6d
Leone Riccardo <riccardo.leone.lavoro@gmail.com>
2024-10-10 12:39:46 +0200

    [bike service] added file to support rabbitMQ

---
+++ src/backend/bike-rental/src/messageBroker/config.ts
@@ -0,0 +1,26 @@
+import dotenv from "dotenv"
+
+dotenv.config()
+
+console.log("RabbitMQ Configurations: ", process.env.RABBITMQ_USER, process.env.RABBITMQ_PASSWORD, process.env.RABBITMQ_HOST)
+
+const rmqUser = process.env.RABBITMQ_USER || "rileone"
+const rmqPass = process.env.RABBITMQ_PASSWORD || "password"
+const rmqhost = process.env.RABBITMQ_HOST || "rabbitMQ"
+
+const REQ_BIKE_QUEUE = "bike_request"
+const REQ_HOTEL_QUEUE = "hotel_request"
+const REQ_PAYMENT_QUEUE = "payment_request"
+const REQ_BOOKING_QUEUE = "booking_request"
+
+const RESP_BIKE_QUEUE = "bike_response"
+const RESP_HOTEL_QUEUE = "hotel_response"
+const RESP_PAYMENT_QUEUE = "payment_response"
+const RESP_BOOKING_QUEUE = "booking_response"
+
+
+export {
+    rmqUser, rmqPass, rmqhost,
+    REQ_BIKE_QUEUE, REQ_HOTEL_QUEUE, REQ_PAYMENT_QUEUE, REQ_BOOKING_QUEUE,
+    RESP_BIKE_QUEUE, RESP_HOTEL_QUEUE, RESP_PAYMENT_QUEUE, RESP_BOOKING_QUEUE
+} 
