82a2cf4dc11d6384426919e235cc74bfc361f89b
Leone Riccardo <riccardo.leone.lavoro@gmail.com>
2024-10-11 15:00:56 +0200

    [bike service] improved logic in handle_cancel_request

--- src/backend/bike-rental/src/messageBroker/handlers.ts
+++ src/backend/bike-rental/src/messageBroker/handlers.ts
@@ -1,7 +1,7 @@
 import { RabbitMQConnection } from "./connection";
 import { order as BikeDO } from "@prisma/client";
 import { BikeOrderRepository, OrderDTO as BikeDTO } from "../api/service/OrderRepository/OrderRepository";
-import { BikeStorageRepository } from "../api/service/StorageRepository/StorageRepository";
+import { BikeDBRepository } from "../api/service/StorageRepository/StorageRepository";
 import { z } from 'zod';
 
 let bike_info_schema = z.object({
@@ -27,16 +27,16 @@ export async function handle_req_from_order_management(rabbitmqClient: RabbitMQC
     order_info = bike_info_schema.parse(description);
   } catch (error) {
     console.error(`[BIKE SERVICE] Error while parsing message:`, error);
-    await rabbitmqClient.sendToOrderManagementMessageBroker("ERROR");
+    await rabbitmqClient.sendToOrderManagementMessageBroker(JSON.stringify({id: "", status: "ERROR"}));
     return;
   }
 
   const manager_db = new BikeOrderRepository();
-  const storage_db = new BikeStorageRepository();
+  const storage_db = new BikeDBRepository();
 
   if (await manager_db.check_existance(order_info.order_id)) {
     console.log("[BIKE SERVICE] Order already exists");
-    await rabbitmqClient.sendToOrderManagementMessageBroker("ERROR");
+    await rabbitmqClient.sendToOrderManagementMessageBroker(JSON.stringify({id: order_info.order_id , status: "ERROR"}));
     return;
   }
   console.log("[BIKE SERVICE] Order does not exist, creating order");
@@ -55,8 +55,8 @@ export async function handle_req_from_order_management(rabbitmqClient: RabbitMQC
     rabbitmqClient.sendToOrderManagementMessageBroker(JSON.stringify(response_info));
     return;
   }
-  console.log("[BIKE SERVICE] Order  with id : ", order.id, "REJECTED, there were not enough bikes");
-  order = await manager_db.update_status(order, "DENIED");
+  console.log("[BIKE SERVICE] Order  with id : ", order.id, "CANCELLED, there were not enough bikes");
+  order = await manager_db.update_status(order, "CANCELLED");
 
   response_info.status = order.renting_status;
 
@@ -64,29 +64,31 @@ export async function handle_req_from_order_management(rabbitmqClient: RabbitMQC
   return;
 }
 
-
 export async function handle_cancel_request(rabbitmqClient: RabbitMQConnection, order_id: string) {
+  let response_info : {id : string |null , status: string | null} =  {
+    id: order_id,
+    status: null,
+  }
   const manager_db = new BikeOrderRepository();
-  const storage_db = new BikeStorageRepository();
+  const storage_db = new BikeDBRepository();
 
   if (await manager_db.check_existance(order_id)) {
-    const order = await manager_db.get_order_info(order_id)!;
+    let order = await manager_db.get_order_info(order_id)!;
     if (order && order.renting_status === "APPROVED") {
       storage_db.increment_bike_count(order.road_bike_requested, order.dirt_bike_requested);
-      const updatedOrder = await manager_db.update_status(order, "CANCELLED");
-
-      const response_info = {
-        id: order_id,
-        status: updatedOrder.renting_status
-      }
+      order = await manager_db.update_status(order, "CANCELLED");
+      response_info.status = order.renting_status,    
       rabbitmqClient.sendToOrderManagementMessageBroker(JSON.stringify(response_info));
-      console.log("[BIKE SERVICE] Order with id: ", order_id, "has been cancelled");
     }
     else {
       console.log("[BIKE SERVICE] Order with id: ", order_id, "is not approved, cannot cancel");
+      response_info.status = "ERROR",
+      rabbitmqClient.sendToOrderManagementMessageBroker(JSON.stringify(response_info))
     }
   } else {
     console.log("[BIKE SERVICE] Order with id: ", order_id, "does not exist");
+    response_info.status = "ERROR"
+    rabbitmqClient.sendToOrderManagementMessageBroker(JSON.stringify(response_info))
   }
 }
 
