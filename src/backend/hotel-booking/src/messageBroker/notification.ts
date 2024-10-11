428dafc36f64b44e329ebc9c3ea72394bfd25b62
Leone Riccardo <riccardo.leone.lavoro@gmail.com>
2024-10-11 14:58:53 +0200

    [hotel service] moved in file for rabbitmq connection

---
+++ src/backend/hotel-booking/src/messageBroker/notification.ts
@@ -0,0 +1,15 @@
+import {rabbitmqClient} from "./connection";
+
+export type INotification = {
+  title: string;
+  description: string;
+};
+
+
+export const sendNotification = async (notification: INotification, queue: string): Promise<boolean> => {
+ 
+  let res = await rabbitmqClient.sendToQueue(queue, notification);
+
+  console.log(`Sent the notification to consumer`);
+  return res;
+};
