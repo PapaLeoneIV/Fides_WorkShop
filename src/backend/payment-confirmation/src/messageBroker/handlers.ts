cf369a2304dc84b46e8723dac6107bd18da763cd
Leone Riccardo <riccardo.leone.lavoro@gmail.com>
2024-10-13 14:52:13 +0200

    [payment service] solved parsing of request

--- src/backend/payment-confirmation/src/messageBroker/handlers.ts
+++ src/backend/payment-confirmation/src/messageBroker/handlers.ts
@@ -5,26 +5,24 @@ import { z } from 'zod';
 const payment_schema = z.object({
     id: z.string(),
     amount: z.string(),
-    created_at: z.date(),
-    updated_at: z.date(),    
+    created_at: z.string().transform((val) => new Date(val)),
+    updated_at: z.string().transform((val) => new Date(val)),
 });
 
 export async function handle_req_from_order_management(instance: RabbitMQConnection, msg: string) {
-    let response_info = {id : "", status: ""};
+    let response_info = { id: "", status: "" };
     let data;
     try {
-        
         const parsedMsg = JSON.parse(msg);
-        console.log("PARSED MSSGGGG::::::::", parsedMsg.description);
         data = payment_schema.parse(JSON.parse(parsedMsg.description));
     } catch (error) {
         console.error(`[PAYMENT SERVICE] Error while parsing message:`, error);
-        //send ERROR to order management
-        return; 
+        instance.sendToOrderMessageBroker(JSON.stringify({ id: "", status: "ERROR" }));
+        return;
     }
     response_info.id = data.id;
     response_info.status = Math.random() < 0.9 ? "APPROVED" : "DENIED";
     //send response to order management 
-
+    instance.sendToOrderMessageBroker(JSON.stringify(response_info));
 
 }
