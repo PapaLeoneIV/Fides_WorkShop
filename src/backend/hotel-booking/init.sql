084ad99bafac40733e92a210291d03bfba00a9dc
Leone Riccardo <riccardo.leone.lavoro@gmail.com>
2024-10-13 14:52:56 +0200

    [init.sql hotel db] modified so that there are more rooms available at the start

--- src/backend/hotel-booking/init.sql
+++ src/backend/hotel-booking/init.sql
@@ -34,7 +34,7 @@ FROM generate_series(0, (24 * 30) - 1) AS t(i);
 INSERT INTO room (room_number, is_available, date_id)
 SELECT 
     ('10' || r)::VARCHAR,              
-    (random() > 0.5)::BOOLEAN,           
+    (random() > 0.1)::BOOLEAN,           
     d.id                                
 FROM date d
 CROSS JOIN generate_series(1, 5) AS r;   
