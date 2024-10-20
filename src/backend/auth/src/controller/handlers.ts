78af9264478b13ccd68f7f37b55d086988bb57ae
Leone Riccardo <riccardo.leone.lavoro@gmail.com>
2024-10-20 12:36:14 +0200

    [auth service] added logic to hash password

--- src/backend/auth/src/controller/handlers.ts
+++ src/backend/auth/src/controller/handlers.ts
@@ -1,6 +1,8 @@
 import { UserManager, UserDTO } from "../service/UserRepository";
 import { RabbitClient } from "../router/rabbitMQClient";
 import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
+
 
 
 
@@ -8,6 +10,9 @@ import bcrypt from 'bcrypt';
 //name shold be 3-20 characters
 //email should be valid
 //password should be 8-20 characters
+
+
+
 
 export async function handle_registration_req(msg: string) {
 
@@ -33,20 +38,40 @@ export async function handle_registration_req(msg: string) {
     return;
 }
 
+
+//TODO for each login request, check if the user should be an admin
+// if so, attach a token with admin privilages
+function generateAccessToken(username: string) {
+    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+  }
+
 export async function handle_login_req(msg: string) {
     const data: UserDTO = JSON.parse(msg);
 
     const User_manager = new UserManager();
     const rabbitmqClient = new RabbitClient();
 
-    let user = await User_manager.get_user(data.username);
-    if (user && ((user.password === data.password && user.username === data.username) 
-        || (user.password === data.password && user.email === data.email))) {
-        console.log("[AUTH SERVICE] User logged in successfully");
-        rabbitmqClient.sendLoginResp(JSON.stringify({ success: "User logged in successfully" }));
+    if (await User_manager.check_existance(data.username)) {
+        console.log("[AUTH SERVICE] User exists");
+        let hashedPassword = await User_manager.get_user_password(data.username);
+        if (hashedPassword) {
+            if (await bcrypt.compare(data.password, hashedPassword)) {
+                console.log("[AUTH SERVICE] User logged in successfully");
+
+                //TODO devo mandare il JWT token
+                rabbitmqClient.sendLoginResp(JSON.stringify({ success: "User logged in successfully" }));
+                return;
+            }
+            else {
+                console.error("[AUTH SERVICE] Wrong password");
+                rabbitmqClient.sendLoginResp(JSON.stringify({ error: "Wrong password" }));
+                return;
+            }
+        }
+    }
+    else {
+        console.error("[AUTH SERVICE] User does not exist");
+        rabbitmqClient.sendLoginResp(JSON.stringify({ error: "User does not exist" }));
         return;
     }
-    console.error("[AUTH SERVICE] Invalid credentials");
-    rabbitmqClient.sendLoginResp(JSON.stringify({ error: "Invalid credentials" }));
-    return;
 }
--- src/backend/auth/src/service/UserRepository.ts
+++ src/backend/auth/src/service/UserRepository.ts
@@ -1,4 +1,5 @@
 import { PrismaClient, registered_users as UserDO } from "@prisma/client";
+import bcrypt from 'bcrypt';
 
 export interface UserDTO {
     username: string;
@@ -16,11 +17,35 @@ export class UserManager{
         return await this.prisma.registered_users.create({
             data: {
                 username: data.username,
-                password: data.password,
+                password: await bcrypt.hash(data.password, process.env.BYCRYPT_SALT),
                 email: data.email
             }
         });
     }
+    }
+
+    async check_password(username: string, password: string) : Promise<boolean>{
+        let user = await this.prisma.registered_users.findUnique({
+            where: {
+                username: username
+            }
+        });
+        if(user){
+            return bcrypt.compare(password, user.password);
+        }
+        return false;
+    }
+
+    async get_user_password(username: string) : Promise<string>{
+        let user = await this.prisma.registered_users.findUnique({
+            where: {
+                username: username
+            }
+        });
+        if(user){
+            return user.password;
+        }
+        return "";
     }
 
     async check_existance(username: string) : Promise<boolean>{
