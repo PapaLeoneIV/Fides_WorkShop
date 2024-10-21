5a5e167be7b88a58dd80a904c40dc4207b4ade34
Riccardo Leone <riccardo.leone97@gmail.com>
2024-10-21 16:42:04 +0200

    [auth service] changed json response

--- src/backend/auth/src/controller/handlers.ts
+++ src/backend/auth/src/controller/handlers.ts
@@ -1,8 +1,7 @@
 import { UserManager, UserDTO } from "../service/UserRepository";
 import { RabbitClient } from "../router/rabbitMQClient";
 import bcrypt from 'bcrypt';
-import jwt from "jsonwebtoken";
-
+import { generateAccessToken } from "../jwt/jwt";
 
 
 
@@ -23,31 +22,29 @@ export async function handle_registration_req(msg: string) {
 
     if (await User_manager.check_existance(data.username)) {
         console.error("[AUTH SERVICE] User already exists");
-        rabbitmqClient.sendRegistrationResp(JSON.stringify({ error: "User already exists" }));
+        rabbitmqClient.sendRegistrationResp(JSON.stringify({ status:"ERROR" ,error: "User already exists" }));
         return;
     }
 
     let user = await User_manager.register_user(data);
     if (user) {
         console.log("[AUTH SERVICE] User registered successfully");
-        rabbitmqClient.sendRegistrationResp(JSON.stringify({ success: "User registered successfully" }));
+        rabbitmqClient.sendRegistrationResp(JSON.stringify({ status:"APPROVED" ,error: "User registered successfully" }));
         return;
     }
     console.log("[AUTH SERVICE] Failed to register user");
-    rabbitmqClient.sendRegistrationResp(JSON.stringify({ error: "Failed to register user" }));
+    rabbitmqClient.sendRegistrationResp(JSON.stringify({ status: "ERROR" ,error: "Failed to register user" }));
     return;
 }
 
 
 //TODO for each login request, check if the user should be an admin
 // if so, attach a token with admin privilages
-function generateAccessToken(username: string) {
-    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
-  }
+
 
 export async function handle_login_req(msg: string) {
     const data: UserDTO = JSON.parse(msg);
-
+    let token : string;
     const User_manager = new UserManager();
     const rabbitmqClient = new RabbitClient();
 
@@ -59,19 +56,20 @@ export async function handle_login_req(msg: string) {
                 console.log("[AUTH SERVICE] User logged in successfully");
 
                 //TODO devo mandare il JWT token
-                rabbitmqClient.sendLoginResp(JSON.stringify({ success: "User logged in successfully" }));
+                token = generateAccessToken(data.username);
+                rabbitmqClient.sendLoginResp(JSON.stringify({ status:"APPROVED", token: token }));
                 return;
             }
             else {
                 console.error("[AUTH SERVICE] Wrong password");
-                rabbitmqClient.sendLoginResp(JSON.stringify({ error: "Wrong password" }));
+                rabbitmqClient.sendLoginResp(JSON.stringify({ status:"ERROR" ,error: "Wrong password" }));
                 return;
             }
         }
     }
     else {
         console.error("[AUTH SERVICE] User does not exist");
-        rabbitmqClient.sendLoginResp(JSON.stringify({ error: "User does not exist" }));
+        rabbitmqClient.sendLoginResp(JSON.stringify({ status:"ERROR" ,error: "User does not exist" }));
         return;
     }
 }
