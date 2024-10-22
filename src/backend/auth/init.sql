e2a98e8ce168f44e4b54568bd265ee1ae7ad74e9
Riccardo Leone <riccardo.leone97@gmail.com>
2024-10-22 11:11:33 +0200

    [auth service] removed username as necessary data

--- src/backend/auth/init.sql
+++ src/backend/auth/init.sql
@@ -4,13 +4,12 @@ DROP TABLE IF EXISTS admins CASCADE;
 
 CREATE TABLE registered_users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE admins (
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY 
     email VARCHAR(50) NOT NULL
 );
 
