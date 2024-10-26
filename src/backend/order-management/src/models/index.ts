4d1b7df208574c8c613bca1150ed94e48fe9fc3d
Riccardo Leone <riccardo.leone97@gmail.com>
2024-10-26 19:08:17 +0200

    [order service] set up exchanges and routes

--- src/backend/order-management/src/models/index.ts
+++ src/backend/order-management/src/models/index.ts
@@ -1,10 +1,31 @@
-import RabbitClient from "./rabbit";
+import {RabbitPublisher, RabbitSubscriber} from "./rabbit";
 import OrderManagerDB from "./order_manager";
 
-const rabbitmqClient = new RabbitClient();
+
+const REQ_BIKE_QUEUE = "bike_request"
+const REQ_HOTEL_QUEUE = "hotel_request"
+const REQ_PAYMENT_QUEUE = "payment_request"
+const REQ_BOOKING_QUEUE = "booking_request"
+
+const rabbitPub = new RabbitPublisher();
+const rabbitSub = new RabbitSubscriber();
+
+
+rabbitPub.connect();
+
+await rabbitPub.setupExchange("OrderEventExchange", "direct");
+
+rabbitSub.connect();
+
+await rabbitSub.createQueue(REQ_BOOKING_QUEUE);
+await rabbitSub.createQueue(REQ_BIKE_QUEUE);
+await rabbitSub.createQueue(REQ_HOTEL_QUEUE);
+await rabbitSub.createQueue(REQ_PAYMENT_QUEUE);
+
 const orderManagerDB = new OrderManagerDB();
 
 export {
-    rabbitmqClient,
+    rabbitPub,
+    rabbitSub,
     orderManagerDB
 }
