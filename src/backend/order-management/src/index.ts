b25041ea0d91d0463428350e32fb20a19dfb44e2
Riccardo Leone <riccardo.leone97@gmail.com>
2024-10-26 14:40:26 +0200

    [order service] set upped main to create exchanges

--- src/backend/order-management/src/index.ts
+++ src/backend/order-management/src/index.ts
@@ -1,12 +1,20 @@
 import { connectToDatabase } from './db/db';
 import { rabbitmqClient } from './models/index';
-const port = 3003;
 
 async function main() {
 
    connectToDatabase();
+   //BOOTSTRAP RABBITMQ
    await rabbitmqClient.connect();
+   await rabbitmqClient.setupEventExchange("OrderEventExchange", "fanout");
+   await rabbitmqClient.setupEventExchange("BookingEventExchange", "fanout");
 
+
+   //PUBLISH
+   rabbitmqClient.publishEvent("OrderEventExchange", "OrderCreated", {id: "123", status: "PENDING"});
+   rabbitmqClient.publishEvent("BookingEventExchange", "BookingCreated", {id: "123", status: "PENDING"});
+
+   //CONSUME
    rabbitmqClient.consumeBookingOrder();
    rabbitmqClient.consumeBikeResponse();
    rabbitmqClient.consumeHotelResponse();
