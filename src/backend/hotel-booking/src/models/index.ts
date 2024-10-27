4e4924feff9ba52728a278ad26d20e6f2391f189
Riccardo Leone <riccardo.leone97@gmail.com>
2024-10-27 14:22:32 +0100

    [hotel booking] added logic for pub/sub rabbitmq

--- src/backend/hotel-booking/src/models/index.ts
+++ src/backend/hotel-booking/src/models/index.ts
@@ -1,12 +1,34 @@
-import RabbitClient         from "./rabbit";
+import {RabbitPublisher, RabbitSubscriber} from "./rabbit";
 import HotelOrderRepository from "./order_manager";
 import HotelDBRepository    from "./storage_manager";
+const HOTEL_SERVICE_ORDER_REQ_QUEUE = "hotel_service_hotel_request"
+const HOTEL_SERVICE_SAGA_REQ_QUEUE = "hotel_service_saga_hotel_request"
 
-const rabbitmqClient = new RabbitClient();
+
+const rabbitPub = new RabbitPublisher();
+const rabbitSub = new RabbitSubscriber();
 const order_manager = new HotelOrderRepository();
 const storage_db = new HotelDBRepository();
+
+
+
+export async function bootstrapRabbitConfig() {
+    console.log("[HOTEL SERVICE]PUB Connecting to RabbitMQ...");
+    await rabbitPub.connect();
+    console.log("[HOTEL SERVICE]SUB Connecting to RabbitMQ...");
+
+    await rabbitSub.connect();
+    console.log("[HOTEL SERVICE]Connected to RabbitMQ");
+
+    console.log("[HOTEL SERVICE]Setting up queues");
+    console.log("[BIKE SERVICE]Setting up queues");
+    await rabbitSub.createQueue(HOTEL_SERVICE_ORDER_REQ_QUEUE);
+    await rabbitSub.createQueue(HOTEL_SERVICE_SAGA_REQ_QUEUE);
+}
+
 export {
-    rabbitmqClient,
+    rabbitPub,
+    rabbitSub,
     order_manager,
     storage_db
 }
--- src/backend/hotel-booking/src/models/rabbit.ts
+++ src/backend/hotel-booking/src/models/rabbit.ts
@@ -1,18 +1,14 @@
 import client, { Connection, Channel } from "amqplib";
-import { handle_req_from_order_management, handle_cancel_request} from "../controller/handlers";
+import { handle_req_from_order_management, handle_cancel_request } from "../controller/handlers";
 
 const rmqUser = process.env.RABBITMQ_USER || "rileone"
 const rmqPass = process.env.RABBITMQ_PASSWORD || "password"
 const rmqhost = process.env.RABBITMQ_HOST || "rabbitmq"
 
-const REQ_HOTEL_QUEUE = "hotel_request"
-
-const RESP_HOTEL_QUEUE = "hotel_response"
-
-const SAGA_RESP_HOTEL_QUEUE = "saga_hotel_response"
+const HOTEL_SERVICE_ORDER_REQ_QUEUE = "hotel_service_hotel_request"
+const HOTEL_SERVICE_SAGA_REQ_QUEUE = "hotel_service_saga_hotel_request"
 
 type HandlerCB = (msg: string, instance?: RabbitClient) => any;
-
 class RabbitClient {
   connection!: Connection;
   channel!: Channel;
@@ -23,22 +19,54 @@ class RabbitClient {
     else this.connected = true;
 
     try {
-      console.log(`[HOTEL SERVICE] Connecting to Rabbit-MQ Server`);
+      console.log(`[ORDER SERVICE] Connecting to Rabbit-MQ Server`);
       this.connection = await client.connect(
         `amqp://${rmqUser}:${rmqPass}@${rmqhost}:5672`
       );
 
-      console.log(`[HOTEL SERVICE] Rabbit MQ Connection is ready`);
+      console.log(`[ORDER SERVICE] Rabbit MQ Connection is ready`);
 
       this.channel = await this.connection.createChannel();
 
-      console.log(`[HOTEL SERVICE] Created RabbitMQ Channel successfully`);
+      console.log(`[ORDER SERVICE] Created RabbitMQ Channel successfully`);
     } catch (error) {
       console.error(error);
-      console.error(`[HOTEL SERVICE]Not connected to MQ Server`);
+      console.error(`[ORDER SERVICE]Not connected to MQ Server`);
     }
   }
+  async setupExchange(exchange: string, exchangeType: string) {
 
+    try {
+      // Declare a fanout exchange
+      await this.channel.assertExchange(exchange, exchangeType, {
+        durable: true,
+      });
+      console.log(`[ORDER SERVICE] Event Exchange '${exchange}' declared`);
+    } catch (error) {
+      console.error(`[ORDER SERVICE] Error setting up event exchange:`, error);
+    }
+  }
+  async publishEvent(exchange: string, routingKey: string, message: any): Promise<boolean> {
+
+    try {
+      if (!this.channel) {
+        await this.connect();
+      }
+
+      return this.channel.publish(
+        exchange,
+        routingKey, // No routing key needed for fanout
+        Buffer.from(message),
+        {
+          //TODO se necessario continuare a customizzare il channel
+          appId: "OrderService",
+        }
+      );
+    } catch (error) {
+      console.error("[ORDER SERVICE] Error publishing event:", error);
+      throw error;
+    }
+  }
   async sendToQueue(queue: string, message: any): Promise<boolean> {
     try {
       if (!this.channel) {
@@ -47,23 +75,30 @@ class RabbitClient {
 
       return this.channel.sendToQueue(queue, Buffer.from(message));
     } catch (error) {
-      console.error("[HOTEL SERVICE]", error);
+      console.error("[ORDER SERVICE]", error);
       throw error;
     }
 
   }
 
-  async consume(queue: string, handlerFunc: HandlerCB) {
+  async createQueue(queue: string) {
     await this.channel.assertQueue(queue, {
       durable: true,
     });
+  }
+  async consume(queue: string, exchange: string, routingKey: string, handlerFunc: HandlerCB) {
+
+    if (!this.channel) {
+      await this.connect();
+    }
+    await this.channel.bindQueue(queue, exchange, routingKey);
 
     this.channel.consume(
       queue,
-      (msg : any) => {
+      (msg) => {
         {
           if (!msg) {
-            return console.error(`[HOTEL SERVICE]Invalid incoming message`);
+            return console.error(`Invalid incoming message`);
           }
           handlerFunc(msg?.content?.toString());
           this.channel.ack(msg);
@@ -75,24 +110,40 @@ class RabbitClient {
     );
   }
 
-  //----------------------CONSUME-------------------------------
-  consumeHotelOrder = async () => {
-    console.log("[HOTEL SERVICE] Listening for Hotel orders...");
-    this.consume(RESP_HOTEL_QUEUE, (msg) => handle_req_from_order_management(msg));
-  };
-
-  //----------------------SEND----------------------------------
-  sendToOrderManagementMessageBroker = async (body: string): Promise<void> => {
-    this.sendToQueue(REQ_HOTEL_QUEUE, body);
-  };
-
-  //----------------------SAGA(CANCEL)--------------------------
-  consumecancelHotelOrder = async () => {
-    this.consume(SAGA_RESP_HOTEL_QUEUE, (msg) => handle_cancel_request(msg)); 
-  }
-
-  
 
 }
 
-export default RabbitClient;
+
+class RabbitPublisher extends RabbitClient {
+  //----------------------SEND----------------------------------
+  sendToOrderManagementMessageBroker = async (body: string): Promise<void> => {
+    console.log(`[HOTEL SERVICE] Sending to Order Management Service: ${body}`);
+    const routingKey = "hotel_main_listener";
+    this.publishEvent("OrderEventExchange", routingKey, body);
+  }
+  sendToOrderManagementMessageBrokerSAGA = async (body: string): Promise<void> => {
+    console.log(`[HOTEL SERVICE] Sending to Order Management Service: ${body}`);
+    const routingKey = "hotel_saga_listener";
+    this.publishEvent("OrderEventExchange", routingKey, body);
+  }
+
+}
+
+class RabbitSubscriber extends RabbitClient {
+  consumeHotelOrder = async () => {
+    console.log("[BIKE SERVICE] Listening for bike orders...");
+    const routingKey = "BDhotel_request";
+    this.consume(HOTEL_SERVICE_ORDER_REQ_QUEUE, "OrderEventExchange", routingKey, (msg) => handle_req_from_order_management(msg));
+  };
+  consumecancelHotelOrder = async () => {
+    console.log("[BIKE SERVICE] Listening for bike orders...");
+    const routingKey = "BDhotel_SAGA_request";
+    this.consume(HOTEL_SERVICE_SAGA_REQ_QUEUE, "OrderEventExchange", routingKey, (msg) => handle_cancel_request(msg));
+  };
+}
+
+export {
+  RabbitClient,
+  RabbitPublisher,
+  RabbitSubscriber
+};
