722ac84a82a0a3b8b78784de53dab6a15f683474
Riccardo Leone <riccardo.leone97@gmail.com>
2024-11-07 17:19:04 +0100

    [auth service] modified expire date jwt

--- src/backend/auth/src/jwt/jwt.ts
+++ src/backend/auth/src/jwt/jwt.ts
@@ -4,12 +4,12 @@ interface UserPayload extends JwtPayload {
     email: string;
 }
 export function generateAccessToken(email: string): string {
-    return jwt.sign({email}, process.env.TOKEN_SECRET as string, { expiresIn: 1 } as jwt.SignOptions);
+    return jwt.sign({email}, process.env.TOKEN_SECRET as string, { expiresIn: 3000 } as jwt.SignOptions);
 }
 
 export function authenticateToken(token: string) {
     try {
-        const decoded = jwt.verify(token, process.env.TOKEN_SECRET as string) as UserPayload;
+        let decoded = jwt.verify(token, process.env.TOKEN_SECRET as string) as UserPayload;
         return decoded;
     } catch (error) {
         console.error("[TOKEN SERVICE] Invalid JWT Token:", error);
