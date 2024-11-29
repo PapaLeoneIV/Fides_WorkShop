317d5e44df710dd416a33185357415143c51cb15
emma <minestrinad@gmail.com>
2024-11-29 17:05:27 +0100

    [hotel] update database to add userEmail to order table

--- src/backend/hotel-booking/init.sql
+++ src/backend/hotel-booking/init.sql
@@ -6,6 +6,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE "order" (
     "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     "order_id" TEXT NOT NULL,
+    "userEmail" TEXT NOT NULL,
     "to" TEXT NOT NULL,
     "from" TEXT NOT NULL,
     "room" TEXT NOT NULL,
